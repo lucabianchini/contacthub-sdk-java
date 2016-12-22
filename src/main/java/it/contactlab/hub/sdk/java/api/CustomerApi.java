698d0c00cca0b09ca484fe6594a7b4d59cb51656
Francesco Negri <francesconegri@gmail.com>
2016-12-22 12:58:58 +0100

    Abstract Gson config to separate class

--- src/main/java/it/contactlab/hub/sdk/java/api/CustomerApi.java
+++ src/main/java/it/contactlab/hub/sdk/java/api/CustomerApi.java
@@ -1,13 +1,10 @@
 package it.contactlab.hub.sdk.java;
 
 import it.contactlab.hub.sdk.java.exceptions.HttpException;
+import it.contactlab.hub.sdk.java.gson.ContactHubGson;
 import it.contactlab.hub.sdk.java.models.Customer;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
@@ -15,59 +12,11 @@ import com.mashape.unirest.http.Unirest;
 import org.json.JSONObject;
 
 import java.lang.reflect.Type;
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class CustomerApi {
 
-  /*
-   * Handling java.time.OffsetDateTime
-   */
-  private static DateTimeFormatter dateTimeFormatter =
-      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
-
-  private static JsonSerializer<OffsetDateTime> dateTimeJsonSerializer =
-      (src, typeOfSrc, context) -> new JsonPrimitive(src.format(dateTimeFormatter));
-  private static JsonDeserializer<OffsetDateTime> dateTimeJsonDeserializer =
-      (json, typeOfT, context) ->
-        json == null ? null : OffsetDateTime.parse(json.getAsString(), dateTimeFormatter);
-
-  /*
-   * Handling java.time.LocalDate
-   */
-  private static DateTimeFormatter dateFormatter =
-      DateTimeFormatter.ISO_LOCAL_DATE; // "yyyy-MM-dd"
-
-  private static JsonSerializer<LocalDate> dateJsonSerializer =
-      (src, typeOfSrc, context) -> new JsonPrimitive(src.format(dateFormatter));
-  private static JsonDeserializer<LocalDate> dateJsonDeserializer =
-      (json, typeOfT, context) ->
-        json == null ? null : LocalDate.parse(json.getAsString(), dateFormatter);
-
-  /*
-   * Handling java.time.ZoneId
-   */
-  private static JsonSerializer<ZoneId> zoneIdJsonSerializer =
-      (src, typeofSrc, context) -> new JsonPrimitive(src.getId());
-  private static JsonDeserializer<ZoneId> zoneIdJsonDeserializer =
-      (json, typeOfT, context) ->
-      json == null ? null : ZoneId.of(json.getAsString());
-
-  /*
-   * Registering all (de)serializers
-   */
-  private static Gson gson =
-      new GsonBuilder()
-      .registerTypeAdapter(OffsetDateTime.class, dateTimeJsonDeserializer)
-      .registerTypeAdapter(OffsetDateTime.class, dateTimeJsonSerializer)
-      .registerTypeAdapter(LocalDate.class, dateJsonSerializer)
-      .registerTypeAdapter(LocalDate.class, dateJsonDeserializer)
-      .registerTypeAdapter(ZoneId.class, zoneIdJsonSerializer)
-      .registerTypeAdapter(ZoneId.class, zoneIdJsonDeserializer)
-      .create();
+  private static Gson gson = ContactHubGson.getInstance();
 
   private static String baseUrl = "https://api.contactlab.it/hub/v1";
 
---
+++ src/main/java/it/contactlab/hub/sdk/java/gson/ContactHubGson.java
@@ -0,0 +1,64 @@
+package it.contactlab.hub.sdk.java.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+public class ContactHubGson {
+
+  /*
+   * Handling java.time.OffsetDateTime
+   */
+  private static DateTimeFormatter dateTimeFormatter =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+
+  private static JsonSerializer<OffsetDateTime> dateTimeJsonSerializer =
+      (src, typeOfSrc, context) -> new JsonPrimitive(src.format(dateTimeFormatter));
+  private static JsonDeserializer<OffsetDateTime> dateTimeJsonDeserializer =
+      (json, typeOfT, context) ->
+        json == null ? null : OffsetDateTime.parse(json.getAsString(), dateTimeFormatter);
+
+  /*
+   * Handling java.time.LocalDate
+   */
+  private static DateTimeFormatter dateFormatter =
+      DateTimeFormatter.ISO_LOCAL_DATE; // "yyyy-MM-dd"
+
+  private static JsonSerializer<LocalDate> dateJsonSerializer =
+      (src, typeOfSrc, context) -> new JsonPrimitive(src.format(dateFormatter));
+  private static JsonDeserializer<LocalDate> dateJsonDeserializer =
+      (json, typeOfT, context) ->
+        json == null ? null : LocalDate.parse(json.getAsString(), dateFormatter);
+
+  /*
+   * Handling java.time.ZoneId
+   */
+  private static JsonSerializer<ZoneId> zoneIdJsonSerializer =
+      (src, typeofSrc, context) -> new JsonPrimitive(src.getId());
+  private static JsonDeserializer<ZoneId> zoneIdJsonDeserializer =
+      (json, typeOfT, context) ->
+      json == null ? null : ZoneId.of(json.getAsString());
+
+  /*
+   * Registering all (de)serializers
+   */
+  public static Gson getInstance() {
+      GsonBuilder gsonBuilder = new GsonBuilder()
+      .registerTypeAdapter(OffsetDateTime.class, dateTimeJsonDeserializer)
+      .registerTypeAdapter(OffsetDateTime.class, dateTimeJsonSerializer)
+      .registerTypeAdapter(LocalDate.class, dateJsonSerializer)
+      .registerTypeAdapter(LocalDate.class, dateJsonDeserializer)
+      .registerTypeAdapter(ZoneId.class, zoneIdJsonSerializer)
+      .registerTypeAdapter(ZoneId.class, zoneIdJsonDeserializer);
+
+      return gsonBuilder.create();
+  }
+
+}
