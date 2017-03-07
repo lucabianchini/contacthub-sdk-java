fb5476c9378f16e0df019c40512296cf749b599b
Francesco Negri <francesconegri@gmail.com>
2017-03-07 13:04:01 +0100

    Add JavaDoc to HttpException

--- src/main/java/it/contactlab/hub/sdk/java/exceptions/HttpException.java
+++ src/main/java/it/contactlab/hub/sdk/java/exceptions/HttpException.java
@@ -4,6 +4,10 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class HttpException extends ContactHubException {
 
+  /**
+   * HttpException is thrown when a request to the ContactHub API failed with an
+   * exception and no response is available.
+   */
   public HttpException(UnirestException exception) {
     super("The HTTP call failed with error: " + exception.getMessage(), exception);
   }
