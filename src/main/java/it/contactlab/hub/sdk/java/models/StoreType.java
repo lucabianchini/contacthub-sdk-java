47d4081dc77ff8eb0d390ed1b74bc7bbe82569e3
Matteo Turri <matteo.turri@buildo.io>
2018-05-22 15:40:53 +0200

    closes #94: Correct serialization for StoreType

--- src/main/java/it/contactlab/hub/sdk/java/models/StoreType.java
+++ src/main/java/it/contactlab/hub/sdk/java/models/StoreType.java
@@ -1,14 +1,16 @@
 package it.contactlab.hub.sdk.java.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum StoreType {
   AIRPORT,
   ECOMMERCE,
   FLAGSHIP,
-  FREE_STANDING,
+  @SerializedName("FREE-STANDING") FREE_STANDING,
   MALL,
   OUTLET,
   RESORT,
   SIS,
   WAREHOUSE,
-  NOT_DEFINED
+  @SerializedName("NOT-DEFINED") NOT_DEFINED
 }
